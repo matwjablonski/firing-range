56345b42a569ad817222907eebe33e854cbd62d8
Mateusz Jabłoński <mat.w.jablonski@gmail.com>
2019-03-04 09:22:45 +0100

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.post('/', function(req, res) {
     console.log(req);
@@ -10,4 +11,4 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.listen(8080);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
